78b8cc8569d64d983189b36e561083c50dd6aaf1
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2014-08-26 02:06:29 -0300

    Added support for the final stages of the wedding: - time and place information - digital invitation - confirmation

--- js/global.js
+++ js/global.js
@@ -134,5 +134,94 @@ $(window).load(function () {
         return false;
     });
 
+    var showAnotherPerson = function(index){
+        var $newPerson = '<div class="person empty" style="display:none;" onkeyup="" index="'+index+'"><div class="separator"></div><input class="subscribe-box" type="text" placeholder="Otro nombre" value="" name="name['+index+']"/><input type="hidden" name="email['+index+']" value=""/></div>';
+        $('form').append($newPerson);
+        $('form .person:last-child').fadeIn();
+        $('input').unbind().keyup(onInputKeyUp);
+    };
+
+    var onInputKeyUp = function(event){
+
+        var $person = $(event.currentTarget).parents('.person');
+        var nextIndex = parseInt($person.attr('index'))+1;
+
+        if ($person.hasClass('first')) {
+            var name = $person.find('input')[1].value;
+            var email = $person.find('input')[2].value;
+            
+            if (name !== '' && email !== '') {
+                console.log(name + ' ' + email);
+                if ($('.person[index='+nextIndex+']').length == 0) {
+                    showAnotherPerson(nextIndex);
+                }
+            } else {
+                $('.person:not(.first)').fadeOut(function(){
+                    $(this).remove();
+                });
+            }
+        }
+        else {
+            var name = $person.find('input')[1].value;
+            if (name !== '') {
+                $person.removeClass('empty');
+                if ($('.person[index='+nextIndex+']').length == 0) {
+                    showAnotherPerson(nextIndex);
+                }
+            } else {
+                $person.addClass('empty');
+                $('.person[index='+nextIndex+']').fadeOut(function(){
+                    $(this).remove();
+                });
+            }
+        }
+    };
+
+    var confirm = function(){
+
+        $('button').hide();
+
+        var arr = [];
+        var mainPersonName = $('.person:first input:first').val();
+        $('form .person').each(function(index){
+            if ($(this).find('input[name^=name]').val() !== '') {
+                arr.push({
+                    name: $(this).find('input[name^=name]').val(),
+                    email: $(this).find('input[name^=email]').val(),
+                    related: mainPersonName
+                });
+            }
+        });
+        var obj = {toma:arr};
+
+        $.ajax({
+            type: 'post',
+            url: 'http://app.konacloud.io/api/airuleguy/casamiento/mr_confirmacion',
+            data: JSON.stringify(obj),
+            success: function(){
+                showMessage('Te esperamos!', true);
+            },
+            error: function(){
+                showMessage("Nuestra paloma mensajera est&aacute; de paro en este momento. Vuelve a intentarlo mas tarde.", false);
+            }
+        });
+    };
+
+    var showMessage = function(msg, ok) {
+        var fn = null;
+        if (!ok) {
+            $('button').show();
+            fn = function(){
+                setTimeout(function(){
+                    $('#message').fadeOut();
+                }, 3500);
+            }
+        }
+        $('#message').html(msg).removeClass().addClass(ok ? 'good' : 'bad').fadeIn(fn);
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    };
+
+    $('input').keyup(onInputKeyUp);
+
 })(jQuery);
 
